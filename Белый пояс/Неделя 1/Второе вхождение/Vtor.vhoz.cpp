140a74e952559fe38cff4d66131b84ff995a4a68
spectrdort <sms1995@rocketmail.com>
2021-05-12 22:46:05 +0300

    Дана строка. Найдите в этой строке второе вхождение буквы f и выведите  индекс этого вхождения. Если буква f в данной строке встречается только  один раз, выведите число -1, а если не встречается ни разу, выведите  число -2. Индексы нумеруются с нуля.

---
+++ Белый пояс/Неделя 1/Второе вхождение/Vtor.vhoz.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+
+int main()
+
+{
+	std::string n = "sasas";
+	// std::cin >> n; //
+	for (int i = 0; i < n.size(); ++i)
+	{
+		if (n[i] == "a")
+		{
+			std::cout << i << std::endl;
+		}
+	}
+	return 0;
+}
