195e970afe63f536839023d35e7426a11708bad4
spectrdort <sms1995@rocketmail.com>
2021-05-12 13:51:19 +0300

    Задача на деление

---
+++ Неделя 1/Деление/Delenie.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+#include <map>
+
+int main() {
+	int a, b, c;
+	std::cin >> a >> b;
+	if (b == 0) {
+		std::cout << "Impossible";
+	}
+	else 
+	{
+		c = a / b;
+		std::cout << c;
+		return 0;
+	}
+}
