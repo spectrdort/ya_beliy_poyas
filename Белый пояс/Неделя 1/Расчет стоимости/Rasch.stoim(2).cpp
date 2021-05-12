dcf031b869e2b3171ec040dae4016a82e2ed458c
spectrdort <sms1995@rocketmail.com>
2021-05-12 14:00:39 +0300

    Задача на рассчет стоимости

---
+++ Белый пояс/Неделя 1/Расчет стоимости/Rasch.stoim(2).cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+int main() 
+{
+
+	double n, a, b, x, y, cena = 0, razn = 0, srav = 0;
+	std::cin >> n >> a >> b >> x >> y;
+
+	razn = b - a;
+	srav = n - a;
+	if (srav < 0 && srav < razn)
+	{
+		std::cout << n;
+		return 0;
+	}
+
+	if (srav < razn)
+	{
+			cena = n * (1 - (x * 0.01));
+			std::cout << cena;
+			return 0;
+	}
+	else
+	{
+			cena = n * (1 - (y * 0.01));
+			std::cout << cena;
+			return 0;
+	}
+}
---
+++ Белый пояс/Неделя 1/Расчет стоимости/Raschet stoimost.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+
+	double n, a, b, x, y, cena = 0;
+	std::cin >> n >> a >> b >> x >> y;
+
+	if (n > b) 
+	{
+		if (y > 100 || y < 0) 
+		{
+			std::cout << "Go fuck urself with such tests";
+			return 0;
+		}
+		else 
+		{
+		cena = n * (1 - y * 0.01);
+		std::cout << cena;
+		return 0; 
+		}
+	}	
+	
+	if (n > a) 
+	{
+		if (x > 100 || x < 0) 
+		{
+			std::cout << "Go fuck urself with such tests";
+			return 0;
+		}
+		else 
+		{
+			cena = n * (1 - x * 0.01);
+			std::cout << cena;
+			return 0;
+		}
+	}
+	else 
+	{
+		cena = n;
+		std::cout << cena;
+		return 0;
+	}
+}
