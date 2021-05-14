27e96434b109d2f682f6f057d9761eb1c50ca65d
spectrdort <sms1995@rocketmail.com>
2021-05-14 12:53:40 +0300

    Задача на двойные числа

---
+++ Белый пояс/Неделя 1/Двойные числа/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	int x;
+	std::cin >> x;
+	std::vector<int> s = { NULL };
+	while (x >= 1)
+	{
+		s.push_back(x % 2);
+		x = x / 2;
+	}
+	for (int n = s.size; n >= 1; --n)
+	{
+		std::cout << s[n];
+	}
+	return 0;
+}
