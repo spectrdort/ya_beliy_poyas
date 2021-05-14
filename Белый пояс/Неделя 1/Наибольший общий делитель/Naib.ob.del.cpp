a510e809cb829ae864b75b3542b5d54573a46928
spectrdort <sms1995@rocketmail.com>
2021-05-14 12:53:57 +0300

    Наибольший общий

---
+++ Белый пояс/Неделя 1/Наибольший общий делитель/Naib.ob.del.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+	int a, b, x = 0;
+	std::cin >> a >> b;
+	for (int i = 1; i <= b; i++)
+	{
+		if (a % i == 0 && b % i == 0)
+		{
+			x = i;
+		}
+	}
+	std::cout << x;
+	return 0;
+}
