0b242ea5578561c53495c8714a14da52ea3c2dc1
spectrdort <sms1995@rocketmail.com>
2021-05-17 00:49:57 +0300

    Отбор полиндромов

---
+++ Белый пояс/Неделя 2/1. Функции/3. Отбор палиндромов/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> PalindromFilter(std::vector<std::string> words, int k)
+{
+	std::string w;
+	std::vector<std::string> res;
+		for (int i = 0; i < words.size(); ++i)
+		{
+			if (words[i].size() >= k)
+			{
+				w = words[i];
+				std::reverse(w.begin(), w.end());
+				{
+					if (w == words[i])
+					{
+						res.push_back(words[i]);
+					}
+				}
+			}
+		}
+	return res;
+}
+
+int main()
+{
+	PalindromFilter({ "weew", "bro", "code", "madam"}, 4);
+	return 0;
+}
