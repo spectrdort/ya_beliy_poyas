5ee766108cc52bcfdc8ac7a23b543b5f652ad901
spectrdort <sms1995@rocketmail.com>
2021-05-17 11:57:23 +0300

    Перемещение строк

---
+++ Белый пояс/Неделя 2/1. Функции/5. Перемещение строк/Perem.strok.cpp
@@ -0,0 +1,9 @@
+﻿#include <vector>
+#include <string>
+
+void MoveStrings(std::vector<std::string>& sourse, std::vector<std::string>& destination)
+{
+	for (int i = 0; i < sourse.size(); ++i)
+		destination.insert(destination.begin(), sourse[i]);
+	sourse.clear();
+}
