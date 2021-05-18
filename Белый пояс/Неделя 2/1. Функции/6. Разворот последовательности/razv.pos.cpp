b928bc525715d04b31cf2981c3a64083984b46fe
spectrdort <sms1995@rocketmail.com>
2021-05-18 20:41:56 +0300

    Последовательность строк

---
+++ Белый пояс/Неделя 2/1. Функции/6. Разворот последовательности/razv.pos.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <vector>
+
+void Reverse(std::vector<int> rev)
+{
+	int h = rev.size();
+	int g = (h - 1) * 2; // сколько всего будет ячеек после добавления //
+	for (int i = 1; i <= g; i++)
+	{
+		rev.insert(rev.begin(), rev[i]);
+		i++;
+	}
+	rev.erase(rev.begin() + h, rev.end());
+}
