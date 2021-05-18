de83dcecc0ff4f595ed27959eb169e5c4294b002
spectrdort <sms1995@rocketmail.com>
2021-05-18 20:58:54 +0300

    Разворот последовательности - 2

---
+++ Белый пояс/Неделя 2/1. Функции/7. Разворот последовательности - 2/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector<int> Reverse(std::vector<int> rev)
+{
+	int h = rev.size();
+	int g = (h - 1) * 2; // сколько всего будет ячеек после добавления //
+	for (int i = 1; i <= g; i++)
+	{
+		rev.insert(rev.begin(), rev[i]);
+		i++;
+	}
+	rev.erase(rev.begin() + h, rev.end());
+	return rev;
+}
