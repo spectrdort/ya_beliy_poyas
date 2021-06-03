42e20e4b9bfa55afdd85af90d4a5890fceb0dbd7
spectrdort <sms1995@rocketmail.com>
2021-06-03 22:03:51 +0300

    //

---
+++ Белый пояс/Неделя 2/3. Контейнер map/4. Автобусные остановки - 2/AutoStop_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <map>
+#include <vector>
+#include <iostream>
+#include <string>
+
+int main()
+{
+	int Q, n = 1;
+	std::map <std::vector<std::string>, int> bus;
+	std::map < std::vector<std::string>, int> :: iterator it = bus.begin();
+	std::vector<std::string> stops;
+	std::string stop;
+	std::cin >> Q;
+	for (int i = 0; i < Q; i++)
+	{
+		int q;
+		std::cin >> q;
+		for (int j = 0; j < q; j++)
+		{
+			std::cin >> stop;
+			stops.push_back(stop);
+		}
+		it = bus.find(stops);
+		if (it == bus.end() || bus.empty())
+		{
+			bus[stops] = n;
+			std::cout << "New bus " << n << std::endl;
+			n++;
+			stops.clear();
+		}
+		else
+		{
+			std::cout << "Already exists for " << it->second << std::endl;
+			stops.clear();
+		}
+	}
+	return 0;
+}
+
