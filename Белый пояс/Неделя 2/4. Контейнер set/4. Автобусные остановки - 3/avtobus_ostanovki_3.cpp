5ee42942cfb9214b0aeb3cf125ffa6daf16d76da
spectrdort <sms1995@rocketmail.com>
2021-06-14 00:59:37 +0300

    Задание "Автобусные остановки - 3"

---
+++ Белый пояс/Неделя 2/4. Контейнер set/4. Автобусные остановки - 3/avtobus_ostanovki_3.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <set>
+#include <map>
+
+int main()
+{
+	int Q;
+	std::cin >> Q;
+	std::map<int, std::set<std::string>> bus;
+	for (int i = 0; i < Q; i++)
+	{
+		int n;
+		std::set<std::string> stops;
+		std::cin >> n;
+		for (int j = 0; j < n; j++)
+		{
+			std::string stop;
+			std::cin >> stop;
+			stops.insert(stop);
+		}
+		if (bus.empty())
+		{
+			bus[1] = stops;
+			std::cout << "New bus 1" << std::endl;
+		}
+		else
+		{
+			auto it = bus.begin();
+			for (it; it != bus.end(); it++)
+			{
+				int w = 0;
+				if (stops != it->second)
+				{
+					w++;
+				}
+				else
+				{
+					std::cout << "Already exists for " << it->first << std::endl;
+					break;
+				}
+				if (w == bus.size())
+				{
+					w++;
+					bus[w] = stops;
+					std::cout << "New bus " << w << std::endl;
+					break;
+				}
+			}
+		}
+	}
+}
