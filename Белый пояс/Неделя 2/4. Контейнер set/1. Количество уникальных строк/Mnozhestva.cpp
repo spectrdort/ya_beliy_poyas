17a89c5b06bf9ba5f05471b7c750fd0a0b37336d
spectrdort <sms1995@rocketmail.com>
2021-06-13 22:38:22 +0300

    Задание "Количество уникальных строк"

---
+++ Белый пояс/Неделя 2/4. Контейнер set/1. Количество уникальных строк/Mnozhestva.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+	int N;
+	std::set<std::string> words;
+	std::cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		std::string word;
+		std::cin >> word;
+		words.insert(word);
+	}
+	std::cout << words.size();
+}
