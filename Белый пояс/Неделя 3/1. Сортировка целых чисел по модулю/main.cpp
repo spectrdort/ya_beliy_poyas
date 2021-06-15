7d05c398d43365788f54bd3090f5ac5117ca0111
spectrdort <sms1995@rocketmail.com>
2021-06-15 12:37:38 +0300

    Задание "Сортировка целых чисел по модулю"

---
+++ Белый пояс/Неделя 3/1. Сортировка целых чисел по модулю/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+bool comp(int& i, int& j)
+{
+		// i > j убывание
+	if (j < 0 && i > 0)
+	{
+			return j * (-1) > i;
+	}
+	else if (i < 0 && j > 0)
+	{
+			return i * (-1) < j;
+	}
+	else if (i < 0 && j < 0)
+	{
+			return i * (-1) < j * (-1);
+	}
+	else
+		return i < j;
+}
+
+int main()
+{
+	int N;
+	std::cin >> N;
+	std::vector<int> nums;
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		std::cin >> num;
+		nums.push_back(num);
+	}
+	std::sort(nums.begin(), nums.end(), comp);
+	for (auto i : nums)
+	{
+		std::cout << i << " ";
+	}
+}
